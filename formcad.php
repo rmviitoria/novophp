89c523a12ad13cad3643c7d5562ff93fa83cad84
rmviitoria <vitoria.mr2000@gmail.com>
2023-04-13 17:07:41 -0300

    phpnovo

---
+++ formcad.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="./index.css">
+	<title>Cadastro de Usuários</title>
+</head>
+<body>
+	
+	<form action="cad.php" method="post">
+	<h1>Formulário de Cadastro</h1>
+		<input type="text" name="nome" id="nome" required placeholder="Nome"><br><br>
+		
+		<input type="email" name="email" id="email" required placeholder="E-mail"><br><br>
+		
+		<input type="password" name="senha" id="senha" required placeholder="Senha"><br><br>
+		
+		<input class="botao" type="submit" value="Enviar">
+	</form>
+</body>
+</html>
